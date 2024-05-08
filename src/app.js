251e52084dcfb8ec905000777dac0fd64e0a21b4
enzogit <enzo_91_10@hotmail.com>
2024-05-08 17:33:04 -0300

    agregados archivos router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
-const app= express();
-const PUERTO= 8080;
-app.use (express.json ());
-app.use (express.urlencoded ({extended:true}));
+const app = express();
+const PUERTO = 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./src/public'))
 
 import exphbs from 'express-handlebars'
@@ -10,16 +10,21 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
+
+//cookie parser & session
 import cookieParser from 'cookie-parser';
+app.use(cookieParser())
 import session from 'express-session';
-
-/* import SocketManager from '../copiado.js';
-new SocketManager(httpServer); */ //
-
-
-
-
-app.get (("/"), (req, res) =>{
-res.send ("Desafío Login Backend")
+import FileStore from 'session-file-store';
+const fileStore = new FileStore(session)
+//usando mongoStorage
+import MongoStore from 'connect-mongo';
+app.get(("/"), (req, res) => {
+    res.send("Desafío Login Backend")
 })
-app.listen (PUERTO, ()=> { console.log(`Escuchando el puerto ${PUERTO}`)})
+
+app.get("/setcookie", (req, res) => {
+
+})
+
+app.listen(PUERTO, () => { console.log(`Escuchando el puerto ${PUERTO}`) })
